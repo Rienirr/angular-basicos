b0b514a3a5f3aa503abfa70a2e59d8181c42d43f
Rienirr <90462305+Rienirr@users.noreply.github.com>
2022-04-05 09:19:29 +0200

    arvhivos

---
+++ src/app/heroes/heroes.module.ts
@@ -0,0 +1,21 @@
+import { CommonModule } from "@angular/common";
+import { NgModule } from "@angular/core";
+import { ContadorModule } from "../contador/contador.module";
+import { HeroeComponent } from "./heroe/heroe.component";
+import { ListadoComponent } from './listado/listado.component';
+
+@NgModule({
+    declarations:[
+        HeroeComponent,
+        ListadoComponent
+    ],
+    exports:[
+        ListadoComponent
+    ],
+    imports:[
+        CommonModule,
+       
+    ]
+})
+
+export class HeroesModule{}
