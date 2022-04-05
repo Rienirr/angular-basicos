23649dab010f2d2430888ea05b9aa64969906e63
Rienirr <90462305+Rienirr@users.noreply.github.com>
2022-04-05 09:26:10 +0200

    cambio contador

--- src/app/contador/contador/contador.component.ts
+++ src/app/contador/contador/contador.component.ts
@@ -13,7 +13,7 @@ import { Component } from '@angular/core';
 export class ContadorComponent {
   titulo: string = 'Contador App';
   numero: number = 10;
-  base: number = 5;
+  base: number = 4;
 
   acumular(valor: number) {
     this.numero += valor;
